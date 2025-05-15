cf16a04abe394fbb631660d6ee2be5b36e331584
Caio Correia <caiovaleriogoulartcorreia@gmail.com>
2025-05-14 23:26:38 -0300

    Correção de conflitos de rotas e build bem-sucedida

--- src/app/avaliacao/nova-avaliacao/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-// Este arquivo define o comportamento de redirecionamento para a rota /avaliacao/nova-avaliacao
-export function GET() {
-  // Redirecionar para a página de nova avaliação
-  return NextResponse.redirect(new URL('/avaliacao/nova', process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'));
-}
